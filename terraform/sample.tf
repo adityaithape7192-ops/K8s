cc99b8fe6a9478fcc7f7bcb217e2541382ad25d5
aditya <adityaithape7192@gmail.com>
2026-03-26 00:46:20 +0530

    terraform update

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -4,7 +4,7 @@ provider "aws"{
 resource "aws_instance" "myinstance" {
     ami = "ami-0f559c3642608c138"
     instance_type = "t3.micro"
-    key_name = "abcd.pem"
+    key_name = "abcd"
     vpc_security_group_ids = [ "sg-0be6230f73360a65c" ]
     tags = {
       name = "aditya"
