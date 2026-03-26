91e1dcc4257f22ebf2bcb4bceff06da99ef53462
aditya <adityaithape7192@gmail.com>
2026-03-27 01:41:02 +0530

    backend added

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -1,21 +1,25 @@
-provider "aws"{
-    region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
 }
+
 terraform {
   backend "s3" {
     bucket = "terraform1112"
+    key    = "terraform.tfstate"
     region = "ap-south-1"
-    }
+  }
 }
+
 resource "aws_instance" "myinstance" {
-    ami = "ami-0f559c3642608c138"
-    instance_type = "t3.micro"
-    key_name = "id_rsa"
-    vpc_security_group_ids = [ "sg-0be6230f73360a65c" ]
-    availability_zone = "ap-south-1"
-    tags = {
-      name = "aditya"
-      Name = "myinstance"
-      Environment = "dev"
-    }
+  ami                    = "ami-0f559c3642608c138"
+  instance_type          = "t3.micro"
+  key_name               = "id_rsa"
+  vpc_security_group_ids = ["sg-0be6230f73360a65c"]
+  availability_zone      = "ap-south-1a"
+
+  tags = {
+    Name        = "myinstance"
+    Environment = "dev"
+    Owner       = "aditya"
+  }
 }
