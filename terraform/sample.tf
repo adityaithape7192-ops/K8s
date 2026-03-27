516cb7636594ca77f280d2aab32fb70c636f9721
aditya <adityaithape7192@gmail.com>
2026-03-28 00:59:23 +0530

    terraform variable added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "myinstance" {
   ami = var.ami_id
   instance_type = var.instance_type_mumbai
   key_name = var.key_name
-  vpc_security_group_ids = [ "var.mysg" ]
+  vpc_security_group_ids = "[var.mysg]"
   availability_zone = var.az
   tags = {
     name  = "suraj"
