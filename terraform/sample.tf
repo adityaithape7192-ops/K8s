e925bf89103cabb16f9216177f5aa823496ab67e
aditya <adityaithape7192@gmail.com>
2026-03-28 01:06:53 +0530

    terraform variable added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,22 +1,24 @@
 provider "aws" {
   region = var.rg
 }
+
 terraform {
   backend "s3" {
     bucket = "terraform12123"
     region = "ap-south-1"
-    key = "tfstatefile"
+    key    = "tfstatefile"
   }
 }
+
 resource "aws_instance" "myinstance" {
-  ami = var.ami_id
-  instance_type = var.instance_type_mumbai
-  key_name = var.key_name
-  vpc_security_group_ids = "[var.mysg]"
-  availability_zone = var.az
+  ami                    = var.ami_id
+  instance_type          = var.instance_type_mumbai
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.mysg]
+  availability_zone      = var.az
+
   tags = {
-    name  = "suraj"
-    Name = "myinstance"
+    Name        = "myinstance"
     Environment = "dev"
   }
 }
