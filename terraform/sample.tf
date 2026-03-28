da3ea549e1a78c31609ea5a34c9d603c3598f3f6
aditya <adityaithape7192@gmail.com>
2026-03-28 22:44:17 +0530

    update mysg

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "myinstance" {
   ami = var.ami_id
   instance_type = var.instance_type_mumbai
   key_name = var.key_name
-  vpc_security_group_ids = "[mysg]"
+  vpc_security_group_ids = ["mysg"]
   availability_zone = var.az
   tags = {
     name  = "aditya"
